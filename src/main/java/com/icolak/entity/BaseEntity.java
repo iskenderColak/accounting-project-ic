c17dac1b2157ad0959b347f80e073eaf49398952
iskenderColak <iclk84@gmail.com>
2022-12-25 22:27:48 +0300

    Base Entity

---
+++ src/main/java/com/icolak/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.icolak.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean isDeleted = false;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
+    private Long insertUserId;
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
+    private Long lastUpdateUserId;
+}
