2b59c7d5d63c50482c9c233ac6055b9ecaab48b3
iskenderColak <iclk84@gmail.com>
2022-12-25 22:52:19 +0300

    InvoiceProduct Entity

---
+++ src/main/java/com/icolak/entity/InvoiceProduct.java
@@ -0,0 +1,35 @@
+package com.icolak.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "invoice_products")
+@Where(clause = "is_deleted = false")
+@NoArgsConstructor
+public class InvoiceProduct extends BaseEntity {
+
+    private int quantity;
+    private BigDecimal price;
+    private int tax;
+    private BigDecimal profitLoss;
+    private int remainingQty;
+
+    @ManyToOne
+    @JoinColumn(name = "invoice_id")
+    private Invoice invoice;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
