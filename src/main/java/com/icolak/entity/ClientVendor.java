fcf054213e8c2cc02621910215d337a249928a70
iskenderColak <iclk84@gmail.com>
2022-12-25 22:54:51 +0300

    Client Vendor Entity

---
+++ src/main/java/com/icolak/entity/ClientVendor.java
@@ -0,0 +1,33 @@
+package com.icolak.entity;
+
+import com.icolak.enums.ClientVendorType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "clients_vendors")
+@Where(clause = "is_deleted = false")
+@NoArgsConstructor
+public class ClientVendor extends BaseEntity {
+
+    private String clientVendorName;
+    private String phone;
+    private String website;
+
+    @Enumerated(EnumType.STRING)
+    private ClientVendorType clientVendorType;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+}
