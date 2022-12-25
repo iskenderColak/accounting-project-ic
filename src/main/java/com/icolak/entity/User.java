d03fe5d3d0ea6a8ac63ac890e28acae3af7cfa98
iskenderColak <iclk84@gmail.com>
2022-12-25 22:47:32 +0300

    User Entity

---
+++ src/main/java/com/icolak/entity/User.java
@@ -0,0 +1,33 @@
+package com.icolak.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@Where(clause = "is_deleted = false")
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private boolean enabled;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+}
