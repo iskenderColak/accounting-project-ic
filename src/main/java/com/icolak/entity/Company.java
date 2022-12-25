5b37a35f2b4473b8d810b92a41c3779a6faa2a16
iskenderColak <iclk84@gmail.com>
2022-12-25 22:42:51 +0300

    Company Entity

---
+++ src/main/java/com/icolak/entity/Company.java
@@ -0,0 +1,31 @@
+package com.icolak.entity;
+
+
+import com.icolak.enums.CompanyStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "companies")
+@Where(clause = "is_deleted = false")
+@NoArgsConstructor
+public class Company extends BaseEntity {
+
+    @Column(unique = true)
+    private String title;
+    private String phone;
+    private String website;
+
+    @Enumerated(EnumType.STRING)
+    private CompanyStatus companyStatus;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
