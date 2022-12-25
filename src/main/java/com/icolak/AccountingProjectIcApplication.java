bc5d7825fb318bf2af7fe845285cdf45058cd36f
iskenderColak <iclk84@gmail.com>
2022-12-25 17:02:23 +0300

    Accounting project

---
+++ src/main/java/com/icolak/AccountingProjectIcApplication.java
@@ -0,0 +1,13 @@
+package com.icolak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProjectIcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingProjectIcApplication.class, args);
+    }
+
+}
