8eb453fcc706198221201a00852e893ffb9aa46f
iskenderColak <iclk84@gmail.com>
2022-12-26 23:53:56 +0300

    UserDTO class

---
+++ src/main/java/com/icolak/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package com.icolak.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private RoleDTO role;
+    private CompanyDTO company;
+    private Boolean isOnlyAdmin; // (should be true if this user is only admin of any company.)
+
+}
