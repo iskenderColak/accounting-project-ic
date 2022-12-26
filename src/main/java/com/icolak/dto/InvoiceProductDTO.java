0d0998cc30a7c6b211f6180762eb11742a247171
iskenderColak <iclk84@gmail.com>
2022-12-26 23:55:59 +0300

    InvoiceProductDTO class

---
+++ src/main/java/com/icolak/dto/InvoiceProductDTO.java
@@ -0,0 +1,24 @@
+package com.icolak.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvoiceProductDTO {
+
+    private Long id;
+    private Integer quantity;
+    private BigDecimal price;
+    private Integer tax;
+    private BigDecimal total;
+    private BigDecimal profitLoss;
+    private Integer remainingQuantity;
+    private InvoiceDTO invoice;
+    private ProductDTO product;
+
+}
