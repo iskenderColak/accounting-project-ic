2ef42e1dff0157bb650f3138a3f2fdd967a63114
iskenderColak <iclk84@gmail.com>
2022-12-29 09:26:47 +0300

    AuthSuccessHandler impl AuthenticationSuccessHandler

---
+++ src/main/java/com/icolak/config/AuthSuccessHandler.java
@@ -0,0 +1,37 @@
+package com.icolak.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@Component
+public class AuthSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+        if (roles.contains("Root User")) {
+            response.sendRedirect("/companies/list");
+        }
+
+        if (roles.contains("Admin")) {
+            response.sendRedirect("/users/list");
+        }
+
+        if (roles.contains("Manager")) {
+            response.sendRedirect("/dashboard");
+        }
+
+        if (roles.contains("Employee")) {
+            response.sendRedirect("/dashboard");
+        }
+    }
+}
