9ce165132f1bf8d386544f718c3d44351696b291
iskenderColak <iclk84@gmail.com>
2022-12-29 09:24:09 +0300

    SecurityServiceImpl loadUserByUsername method

---
+++ src/main/java/com/icolak/service/implementation/SecurityServiceImpl.java
@@ -0,0 +1,30 @@
+package com.icolak.service.implementation;
+
+import com.icolak.entity.User;
+import com.icolak.entity.common.UserPrincipal;
+import com.icolak.repository.UserRepository;
+import com.icolak.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserPrincipal(user);
+    }
+}
