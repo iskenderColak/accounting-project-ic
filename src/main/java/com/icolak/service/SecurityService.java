606724499881979918bcc2c466ca94e5368cf7ae
iskenderColak <iclk84@gmail.com>
2023-01-01 23:29:30 +0300

    Security Service getLoggedInUser

--- src/main/java/com/icolak/service/SecurityService.java
+++ src/main/java/com/icolak/service/SecurityService.java
@@ -1,6 +1,10 @@
 package com.icolak.service;
 
+import com.icolak.dto.UserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface SecurityService extends UserDetailsService {
+
+    UserDTO getLoggedInUser();
+
 }
--- src/main/java/com/icolak/service/implementation/SecurityServiceImpl.java
+++ src/main/java/com/icolak/service/implementation/SecurityServiceImpl.java
@@ -1,9 +1,12 @@
 package com.icolak.service.implementation;
 
+import com.icolak.dto.UserDTO;
 import com.icolak.entity.User;
 import com.icolak.entity.common.UserPrincipal;
 import com.icolak.repository.UserRepository;
 import com.icolak.service.SecurityService;
+import com.icolak.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -12,9 +15,11 @@ import org.springframework.stereotype.Service;
 public class SecurityServiceImpl implements SecurityService {
 
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    public SecurityServiceImpl(UserRepository userRepository) {
+    public SecurityServiceImpl(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     @Override
@@ -26,5 +31,11 @@ public class SecurityServiceImpl implements SecurityService {
             throw new UsernameNotFoundException(username);
         }
         return new UserPrincipal(user);
+    }
+
+    @Override
+    public UserDTO getLoggedInUser() {
+        var currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userService.findByUsername(currentUsername);
     }
 }
