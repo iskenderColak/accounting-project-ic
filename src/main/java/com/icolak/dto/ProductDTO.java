6def66b7007e54b58ae8e5b9ded2e807e55aa590
iskenderColak <iclk84@gmail.com>
2023-01-03 16:39:11 +0300

    Product Dto Class

--- src/main/java/com/icolak/dto/ProductDTO.java
+++ src/main/java/com/icolak/dto/ProductDTO.java
@@ -5,16 +5,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
 
     private Long id;
+    @NotBlank(message = "Required field")
+    @Size(min = 2, max = 50, message = "Product name should be between 2 and 50")
     private String name;
+
     private Integer quantityInStock;
+
+    @NotNull(message = "Required field")
+    @Min(value = 1, message = "Should be minimum 1")
     private Integer lowLimitAlert;
+
+    @NotNull(message = "Required field")
     private ProductUnit productUnit;
+
+    @NotNull(message = "Required field")
     private CategoryDTO category;
 
 }
