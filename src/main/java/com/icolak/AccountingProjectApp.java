8fea39383d2b31c1f956bb3073527f596fbfced2
iskenderColak <iclk84@gmail.com>
2023-01-14 08:55:32 +0300

    @EnableFeignClients

--- src/main/java/com/icolak/AccountingProjectApp.java
+++ src/main/java/com/icolak/AccountingProjectApp.java
@@ -3,11 +3,13 @@ package com.icolak;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AccountingProjectApp {
 
     public static void main(String[] args) {
